ab95d2487cd379430bb4cce28976d93a3dc4b682
Zachary Belford <belfordz66@gmail.com>
2019-02-16 15:28:39 -0800

    fix: formatiing

--- test.js
+++ test.js
@@ -13,12 +13,11 @@ describe('meta-schema', () => {
   describe('validates all examples without error', () => {
     const exampleNames = Object.keys(openRpcExamples);
     exampleNames.forEach((exampleName) => {
-      it(`validates the example : ${exampleName}`, () => {
+      it(`validates the example: ${exampleName}`, () => {
         openRpcExamples[exampleName];
         const result = djv.validate(openRpcExamples[exampleName]);
         expect(result).toBeUndefined();
-
-      })
+      });
     });
   });
 });
