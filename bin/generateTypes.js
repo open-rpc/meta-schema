f0cb299655484dc4b1ecd147e135585661c4205b
Zachary Belford <belfordz66@gmail.com>
2019-03-17 16:01:45 -0700

    feat: typescript it up

---
+++ bin/generateTypes.js
@@ -0,0 +1,16 @@
+const schema = require('../src/schema.json');
+const { compile } = require('json-schema-to-typescript');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const writeFile = promisify(fs.writeFile);
+// errors if you try to run with $ref to draft 7 json schema
+schema.definitions.schema.$ref = undefined;
+
+const go = async () => {
+  const ts = await compile(schema, 'OpenRPC');
+  await writeFile(path.resolve(__dirname, '../src/types.ts'), ts, 'utf8');
+
+  console.log('Generating types complete!');
+};
+go();
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import schema from "./schema.json";
+import * as types from "./types";
+
+export { types };
+export default schema;
