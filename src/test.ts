eabe9731290b264915bc30645145bd7b15e7c044
Zachary Belford <belfordz66@gmail.com>
2020-10-23 12:40:05 -0700

    fix: change to using non-github url for meta schema

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,26 @@
 import * as fs from "fs";
+import Dereferencer from "@json-schema-tools/dereferencer";
+import dereffer from "@json-schema-tools/dereferencer";
 
 describe("meta-schema", () => {
+  const s = fs.readFileSync("./schema.json", "utf8");
+  let schema = false;
+  try {
+    schema = JSON.parse(s);
+  } catch (e) { }
 
   it("is valid json", () => {
-    const schema = fs.readFileSync("./schema.json", "utf8");
-    const asJson = JSON.parse(schema);
-    expect(asJson.type).toBe("object");
+    expect(schema).toBeTruthy();
+  });
+
+  it("has references that are valid", async () => {
+    expect.assertions(4);
+    const deref = new Dereferencer(schema);
+    const dereffed = await deref.resolve();
+    expect(dereffed).toBeTruthy();
+    console.log(dereffed);
+    expect(dereffed.definitions).not.toBeDefined();
+    expect(dereffed.properties.methods.items.properties.result.oneOf[0].properties.schema.title).toBe("JSONSchema");
+    expect(dereffed.properties.methods.items.properties.result.oneOf[1].title).toBe("referenceObject");
   });
 });
